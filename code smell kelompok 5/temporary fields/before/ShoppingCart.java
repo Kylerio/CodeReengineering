6ba6b1ce83b08db219d8ab233dc78daaa39317a7
Kylerio <92713286+Kylerio@users.noreply.github.com>
2023-04-10 20:35:06 +0700

    Create ShoppingCart.java

---
+++ code smell kelompok 5/temporary fields/before/ShoppingCart.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ShoppingCart {
+	
+	private List<Item> items;
+	private TotalCalculator totalCalc;
+
+    // constructor
+    public ShoppingCart() {
+        this.items = new ArrayList<>();
+        this.totalCalc = new TotalCalculator(items);
+    }
+
+    // add item to cart
+    public void addItem(Item item) {
+        items.add(item);
+        totalCalc.updateTotalPrice(item.getPrice());
+    }
+
+    // getter for totalPrice
+    public double getTotalPrice() {
+        return totalCalc.getTotalPrice();
+    }
+
+    public static void main(String[] args) {
+        ShoppingCart cart = new ShoppingCart();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Enter item name (or 'exit' to quit):");
+            String itemName = scanner.nextLine();
+            if (itemName.equals("exit")) {
+                break;
+            }
+            System.out.println("Enter item price:");
+            double itemPrice = Double.parseDouble(scanner.nextLine());
+            Item item = new Item(itemName, itemPrice);
+            cart.addItem(item);
+        }
+        scanner.close();
+        System.out.println("Total price: " + cart.getTotalPrice());
+    }
+}
