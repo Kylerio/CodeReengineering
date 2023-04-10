6536fef7b8333d6ca07dcf04b71bee51dfa79ba8
Kylerio <92713286+Kylerio@users.noreply.github.com>
2023-04-10 21:41:14 +0700

    Create ContactInfo.java

---
+++ code smell kelompok 13/feature envy/after/ContactInfo.java
@@ -0,0 +1,21 @@
+public class ContactInfo {
+    public String GetStreetName()
+        {
+            return "1 Medison Square";
+        }
+        public String GetCity()
+        {
+            return "NewYork";
+        }
+        public String GetState()
+        {
+            return "NY";
+        }
+        public String GetFullAddress()
+        {
+            String city = GetCity();
+            String state = GetState();
+            String streetName = GetStreetName();
+            return streetName + ";" + city + ";" + state;
+        }
+}
