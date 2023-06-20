fab2985a1af6f8e1ff56fb0aa0aca846f695d73f
digidaw <92713286+Kylerio@users.noreply.github.com>
2023-06-20 20:33:31 +0700

    Create ToDoListTest.java

---
+++ girish/Encapsulation Smell/Leaky Encapsulation/after/ToDoListTest.java
@@ -0,0 +1,18 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+public class ToDoListTest {
+    void test() {
+        ToDoList td = new ToDoList();
+
+        td.add(new ToDo("a"));
+        assert (td.getByName("a") != null);
+
+        List<ToDo> list = td.getList();
+        list.get(0).setName("b");
+
+        assert (td.getByName("b") != null);
+    }
+}
