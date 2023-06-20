02eeba0029852b66b89c63c3237afe8f46e188ac
digidaw <92713286+Kylerio@users.noreply.github.com>
2023-06-20 21:08:21 +0700

    Create mahasiswa.java

---
+++ girish/Modulazation Smell/Broken Modularization/after/mahasiswa.java
@@ -0,0 +1,27 @@
+public class mahasiswa {
+    private DataMahasiswa data;
+    
+    public String getNIM() {
+        return data.getNIM();
+    }
+    
+    public void setNIM(String NIM) {
+        data.setNIM(NIM);
+    }
+    
+    public String getEmail() {
+        return data.getEmail();
+    }
+    
+    public void setEmail(String Email) {
+        data.setEmail(Email);
+    }
+    
+    public String getNomorTelepon() {
+        return data.getNomorTelepon();
+    }
+    
+    public void setNomorTelepon(String NomorTelepon) {
+        data.setNomorTelepon(NomorTelepon);
+    }
+}
