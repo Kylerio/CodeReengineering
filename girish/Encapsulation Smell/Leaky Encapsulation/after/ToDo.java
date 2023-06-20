6d6a3cf54102145ae887d927b99eeed1003f2ebe
digidaw <92713286+Kylerio@users.noreply.github.com>
2023-06-20 20:30:27 +0700

    Create ToDo.java

---
+++ girish/Encapsulation Smell/Leaky Encapsulation/after/ToDo.java
@@ -0,0 +1,20 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+class ToDo {
+    private String name;
+
+    public ToDo(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
